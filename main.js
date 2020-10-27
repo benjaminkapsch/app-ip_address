95a2ac039de477b7868c36b869d68cd3f3e074a2
benjaminkapsch <benjamin.kapsch@centurylink.com>
2020-10-27 13:17:53 +0000

    Lab 4

--- main.js
+++ main.js
@@ -31,52 +31,52 @@ class IpAddress {
     // Developer Hub https://developer.itential.io/ located
     // under Documentation -> Developer Guides -> Log Class Guide
     log.info('Starting the IpAddress product.');
-    /**
-    * Calculate and return the first host IP address from a CIDR subnet.
-    * @param {string} cidrStr - The IPv4 subnet expressed
-    *                 in CIDR format.
-    * @param {callback} callback - A callback function.
-    * @return {string} (firstIpAddress) - An IPv4 address.
-    */
-    getFirstIpAddress(cidrStr, callback) {
+	}
+	/**
+	 * Calculate and return the first host IP address from a CIDR subnet.
+	 * @param {string} cidrStr - The IPv4 subnet expressed
+	 *                 in CIDR format.
+	 * @param {callback} callback - A callback function.
+	 * @return {string} (firstIpAddress) - An IPv4 address.
+	 */
+  getFirstIpAddress(cidrStr, callback) {
 
-    // Initialize return arguments for callback
-    let firstIpAddress = null;
-    let callbackError = null;
-    let ipv4_ipv6_add = {
-        "ipv4": null,
-        "ipv6": null
-    };
-
+	// Initialize return arguments for callback
+	let firstIpAddress = null;
+	let callbackError = null;
+	let ipv4_ipv6_add = {
+	 "ipv4": null,
+	 "ipv6": null
+	};
+	
     // Instantiate an object from the imported class and assign the instance to variable cidr.
-    const cidr = new IPCIDR(cidrStr);
-    // Initialize options for the toArray() method.
-    // We want an offset of one and a limit of one.
-    // This returns an array with a single element, the first host address from the subnet.
-    const options = {
-        from: 1,
-        limit: 1
-    };
+	const cidr = new IPCIDR(cidrStr);
+	// Initialize options for the toArray() method.
+	// We want an offset of one and a limit of one.
+	// This returns an array with a single element, the first host address from the subnet.
+	const options = {
+	  from: 1,
+	  limit: 1
+	};
 
-    // Use the object's isValid() method to verify the passed CIDR.
-    if (!cidr.isValid()) {
-        // If the passed CIDR is invalid, set an error message.
-        callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-    } else {
-        // If the passed CIDR is valid, call the object's toArray() method.
-        // Notice the destructering assignment syntax to get the value of the first array's element.
-        [firstIpAddress] = cidr.toArray(options);
-        // Saves the returned ipv4 value to the object to be returned.
-        ipv4_ipv6_add["ipv4"] = firstIpAddress;
-        // Generates and saves the ipv6 value from the ipv4 value to the object to be returned.
-        ipv4_ipv6_add["ipv6"] = getIpv4MappedIpv6Address(firstIpAddress);
-    }
-    // Call the passed callback function.
-    // Node.js convention is to pass error data as the first argument to a callback.
-    // The IAP convention is to pass returned data as the first argument and error
-    // data as the second argument to the callback function.
-    return callback(ipv4_ipv6_add, callbackError);
-    }
+	// Use the object's isValid() method to verify the passed CIDR.
+	if (!cidr.isValid()) {
+	// If the passed CIDR is invalid, set an error message.
+	  callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
+	} else {
+	  // If the passed CIDR is valid, call the object's toArray() method.
+	  // Notice the destructering assignment syntax to get the value of the first array's element.
+	  [firstIpAddress] = cidr.toArray(options);
+	  // Saves the returned ipv4 value to the object to be returned.
+	  ipv4_ipv6_add["ipv4"] = firstIpAddress;
+	  // Generates and saves the ipv6 value from the ipv4 value to the object to be returned.
+	  ipv4_ipv6_add["ipv6"] = getIpv4MappedIpv6Address(firstIpAddress);
+	}
+	// Call the passed callback function.
+	// Node.js convention is to pass error data as the first argument to a callback.
+	// The IAP convention is to pass returned data as the first argument and error
+	// data as the second argument to the callback function.
+	return callback(ipv4_ipv6_add, callbackError);
   }
 }
 
